d34321c536ea68b17e37bda047be729423b5bbd3
Ash <ash@ash.home>
2020-07-24 02:49:50 +0100

    added new dorm form, fixed some names

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -10,75 +10,67 @@ use Illuminate\Http\Request;
 class ReviewsController extends Controller
 {   
 
-        //maybe private universities might work here
-
     public function index(){
         $universities = University::all();
-
         $amenities = array('common_area', 'games','outdoor_area','elevator'
-                    ,'communal_kitchen','catering','private_bathroom'
-                    , 'social_events','mature_students_only');
-
+    ,'communal_kitchen','catering','private_bathroom'
+    , 'social_events','mature_students_only');
         return view('review', compact('universities','amenities'));
     }
 
     public function create(Request $request){
-        var_dump($request->input("amenities"));
         
-        $tempReview = new TempReview();
+        $temp_review = new TempReview();
         
-        $tempReview->dorm_id = $request->input("dorm_id");
-        $tempReview->room_rating = $request->input("room_rating");
-        $tempReview->building_rating = $request->input("building_rating");
-        $tempReview->location_rating = $request->input("location_rating");
-        $tempReview->bathroom_rating = $request->input("bathroom_rating");
-        $tempReview->staff_rating = $request->input("staff_rating");
-        $tempReview->is_recommended = $request->input("is_recommended");
-        $tempReview->year_of_study = $request->input("year_of_study");
-        $tempReview->year_of_residence = $request->input("year_of_residence");
-        $tempReview->room_type = $request->input("room_type");
-        
-        
-        $tempReview->is_catered = $request->input("is_catered");
-        $tempReview->catered_or_selfcatered_rating = $request->input("catered_or_selfcatered_rating");
+        $temp_review->dorm_id = $request->input("dorm_id");
+
+
+        $temp_review->room_rating = $request->input("room_rating");
+        $temp_review->building_rating = $request->input("building_rating");
+        $temp_review->location_rating = $request->input("location_rating");
+        $temp_review->bathroom_rating = $request->input("bathroom_rating");
+        $temp_review->staff_rating = $request->input("staff_rating");
+        $temp_review->is_recommended = $request->input("is_recommended");
+        $temp_review->year_of_study = $request->input("year_of_study");
+        $temp_review->year_of_residence = $request->input("year_of_residence");
+        $temp_review->room_type = $request->input("room_type");
+                
+        $temp_review->is_catered = $request->input("is_catered");
+        $temp_review->catered_or_selfcatered_rating = $request->input("catered_or_selfcatered_rating");
 
         $has_amenities = $request->input("amenities");
 
         if($has_amenities  !== null){
-            $tempReview->amenities = implode(",",$has_amenities);
+            $temp_review->amenities = implode(",",$has_amenities);
         }
-        $tempReview->quirk = $request->input("quirk");
-        $tempReview->review_text = $request->input("review_text");
-
-
-        /*
-        $input = $request->all();
-        foreach($input as $key=>$value){
-            //$tempReview->$key = $request->input(strval($value));
-            //var_dump($tempReview);
-           
-        }
-        */
+        $temp_review->quirk = $request->input("quirk");
+        $temp_review->review_text = $request->input("review_text");
         
-        $tempReview->save();
-
+        $temp_review->save();
     }
 
-    public function dormsOnUniSelection($uniName){
-        $uniID = University::where('uni_name', strval($uniName))->value('uni_id');
-        $dormsForUni = Dorm::where('uni_id', strval($uniID))->get();   
+    public function dormsOnUniSelection($uni_name){
+        $uni_id = University::where('uni_name', strval($uni_name))->value('uni_id');
+        $dorms_of_uni = Dorm::where('uni_id', strval($uni_id))->get();   
 
-        $dormNameArr= array();
-        foreach($dormsForUni as $eh){
-            array_push($dormNameArr, strval($eh->dorm_name));
+        $dorm_name_arr= array();
+        foreach($dorms_of_uni as $dorm){
+            array_push($dorm_name_arr, strval($dorm->dorm_name));
         }
-        return $dormNameArr;
+        return $dorm_name_arr;
     }
 
-    public function dormNameToId($dormName){
-        $dormId = Dorm::where('dorm_name', strval($dormName))->value('dorm_id');
+    public function dormNameToId($dorm_name){
+        $dorm_id = Dorm::where('dorm_name', strval($dorm_name))->value('dorm_id');
         
-        return $dormId;
+        return $dorm_id;
+    }
+
+    public function newDormReviewPage($uni_name){
+        $amenities = array('common_area', 'games','outdoor_area','elevator'
+    ,'communal_kitchen','catering','private_bathroom'
+    , 'social_events','mature_students_only');
+        return view('new_dorm_review', compact('uni_name', 'amenities'));
     }
 
 
